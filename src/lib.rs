eba4e0dd5e7e07f607f567526c8f069855d0adee
Federico Miras <miras.federico@gmail.com>
2023-04-24 11:45:20 -0300

    feat: initial commit + client init

---
+++ src/lib.rs
@@ -0,0 +1,125 @@
+use hyper::{Body, Client as HyperClient, Method, Request};
+use hyper_tls::HttpsConnector;
+use serde::Serialize;
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AuthRequest {
+    client_id: String,
+    client_secret: String,
+    non_expiring: bool,
+}
+
+pub struct Client {
+    client_id: String,
+    client_secret: String,
+    url: String,
+    client: HyperClient<HttpsConnector<hyper::client::connect::HttpConnector>>,
+}
+
+impl Client {
+    pub fn new(client_id: String, client_secret: String) -> Self {
+        let client = HyperClient::builder().build::<_, hyper::Body>(HttpsConnector::new());
+
+        Self {
+            client_id,
+            client_secret,
+            url: "https://api.pluggy.ai".to_string(),
+            client,
+        }
+    }
+
+    pub fn new_from_env() -> Result<Self, Box<dyn std::error::Error>> {
+        let client_id = dotenv::var("PLUGGY_CLIENT_ID")?;
+        let client_secret = dotenv::var("PLUGGY_CLIENT_SECRET")?;
+        let client = Self::new(client_id, client_secret);
+        Ok(client)
+    }
+
+    pub async fn new_from_env_with_api_key() -> Result<(Self, String), Box<dyn std::error::Error>> {
+        let client = Self::new_from_env()?;
+        let connect_token = client.create_api_key().await?;
+        Ok((client, connect_token))
+    }
+
+    async fn create_api_key(&self) -> Result<String, Box<dyn std::error::Error>> {
+        let url = format!("{}/auth", self.url);
+
+        let payload = AuthRequest {
+            client_id: self.client_id.clone(),
+            client_secret: self.client_secret.clone(),
+            non_expiring: false,
+        };
+
+        // Serialize the payload to a JSON string
+        let json_payload = serde_json::to_string(&payload)?;
+
+        // fetch with hyper rs
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri(url)
+            .header("Content-Type", "application/json")
+            .body(Body::from(json_payload))?;
+
+        let response = self.client.request(request).await?;
+        let body = hyper::body::to_bytes(response.into_body()).await?;
+        let body = String::from_utf8(body.to_vec())?;
+        let json: serde_json::Value = serde_json::from_str(&body)?;
+        let api_key = json["apiKey"].as_str();
+
+        match api_key {
+            Some(api_key) => Ok(api_key.to_string()),
+            None => Err("No api key found".into()),
+        }
+    }
+
+    pub async fn create_connect_token(
+        &self,
+        api_key: &str,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let url = format!("{}/connect_token", self.url);
+
+        let request = Request::builder()
+            .method(Method::POST)
+            .uri(url)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .header("X-API-KEY", api_key)
+            .body(Body::empty())?;
+
+        let response = self.client.request(request).await?;
+        let body = hyper::body::to_bytes(response.into_body()).await?;
+        let body = String::from_utf8(body.to_vec())?;
+        let json: serde_json::Value = serde_json::from_str(&body)?;
+        let connect_token = json["accessToken"].as_str();
+
+        print!("{:?}", json);
+
+        match connect_token {
+            Some(connect_token) => Ok(connect_token.to_string()),
+            None => Err("No connect token found".into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_instantiate_from_env() {
+        Client::new_from_env().unwrap();
+    }
+
+    #[tokio::test]
+    async fn can_instantiate_from_env_with_api_key() {
+        Client::new_from_env_with_api_key().await.unwrap();
+    }
+
+    #[tokio::test]
+    async fn can_create_connect_token() {
+        let (client, api_key) = Client::new_from_env_with_api_key().await.unwrap();
+        let connect_token = client.create_connect_token(&api_key).await.unwrap();
+        assert_eq!(connect_token.len(), 892);
+    }
+}
